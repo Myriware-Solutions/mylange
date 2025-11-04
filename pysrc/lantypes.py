4b0756b53ddbd379c743b0ebe9cccf04a8937953
Anthony-Minecraft <anthonyfsmith50@gmail.com>
2025-11-04 12:47:37 -0500

    Created LanType container for types

--- pysrc/lantypes.py
+++ pysrc/lantypes.py
@@ -1,5 +1,5 @@
 # IMPORTS
-from enum import IntEnum, Enum
+from enum import IntEnum, Enum, unique
 from lanregexes import ActualRegex
 import re
 
@@ -9,138 +9,88 @@ if typing.TYPE_CHECKING:
 
 # Type castisting for variables
 
-class VariableValue:
-    type VariableValueLike = None|bool|int|str|list['VariableValue']|dict[str,'VariableValue']|'LanClass'
-    typeid:int
-    def __init__(self, typeid:int, value:VariableValueLike=None):
-        self.typeid = typeid
-        self.value:VariableValue.VariableValueLike = None
-        if (value != None): self.value = value
-
-    def __str__(self):
-        match (self.typeid):
-            case LanTypes.nil:
-                return 'nil'
-            case LanTypes.boolean:
-                return f"{self.value}".lower()
-            case LanTypes.integer:
-                return f"{self.value}"
-            case LanTypes.character:
-                return f"'{self.value}'"
-            case LanTypes.string:
-                return f'"{self.value}"'
-            case LanTypes.array:
-                assert type(self.value) is list
-                larray = [f"{item}" for item in self.value]
-                return f"[{', '.join(larray)}]"
-            case LanTypes.set:
-                assert type(self.value) is dict
-                lset = [f"{k}:{v}" for k, v in self.value.items()]
-                return f"({', '.join(lset)})"
-        return f"<{self.value}@{self.typeid}>"
-    
-    def __repr__(self) -> str:
-        return self.__str__()
-    
-    def isof(self, type:int) -> bool:
-        return self.typeid == type
-    
-    def to_string(self):
-        match (self.typeid):
-            case LanTypes.boolean:
-                return f"{self.value}".lower()
-            case LanTypes.integer:
-                return f"{self.value}"
-            case LanTypes.character | LanTypes.string:
-                return self.value
-            case _:
-                return self.__str__()
-            
-    def colon_access(self, index:str) -> 'VariableValue':
-        from builtinfunctions import ParamChecker
-        ParamChecker.EnsureIntegrety((self, LanTypes.set))
-        assert type(self.value) is dict
-        return self.value[index]
-
-    def bracket_access(self, index:int) -> 'VariableValue':
-        from builtinfunctions import ParamChecker
-        ParamChecker.EnsureIntegrety((self, LanTypes.array))
-        assert type(self.value) is list
-        return self.value[index]
-
 class RandomTypeConversions:
     @staticmethod
-    def convert(string:str, mylangeInterpreter) -> VariableValue:
+    def convert(string:str, mylangeInterpreter) -> 'VariableValue':
         typeid, other = RandomTypeConversions.get_type(string)
         match (typeid):
-            case LanTypes.nil:
-                return VariableValue(LanTypes.nil, None)
-            case LanTypes.boolean if other == 0:
-                return VariableValue(LanTypes.boolean, False)
-            case LanTypes.boolean if other == 1:
-                return VariableValue(LanTypes.boolean, True)
-            case LanTypes.integer:
-                return VariableValue(LanTypes.integer, int(string))
-            case LanTypes.character:
-                return VariableValue(LanTypes.character, string[1:-1])
-            case LanTypes.string:
-                return VariableValue(LanTypes.string, string[1:-1])
-            case LanTypes.array:
+            case LanScaffold.nil:
+                return VariableValue(LanType.nil(), None)
+            case LanScaffold.boolean if other == 0:
+                return VariableValue(LanType.bool(), False)
+            case LanScaffold.boolean if other == 1:
+                return VariableValue(LanType.bool(), True)
+            case LanScaffold.integer:
+                return VariableValue(LanType.int(), int(string))
+            case LanScaffold.character:
+                return VariableValue(LanType.char(), string[1:-1])
+            case LanScaffold.string:
+                return VariableValue(LanType.str(), string[1:-1])
+            
+            case LanScaffold.array:
                 insides:str = string[1:-1]
                 from interpreter import MylangeInterpreter, CodeCleaner
                 mi:MylangeInterpreter = mylangeInterpreter
                 parts:list[str] = [item.strip() for item in CodeCleaner.split_top_level_commas(insides)]
                 ReturnL:list = []
+                ArchetypeTypeIds:list[LanType] = []
                 for item in parts:
                     var = mi.format_parameter(item)
+                    assert type(var) is VariableValue
                     ReturnL.append(var)
-                return VariableValue(LanTypes.array, ReturnL)
-            case LanTypes.set:
+                    if var.Type not in ArchetypeTypeIds: 
+                        ArchetypeTypeIds.append(var.Type)
+                return VariableValue(LanType(LanScaffold.array, ArchetypeTypeIds), ReturnL)
+            
+            case LanScaffold.set:
                 insides:str = string[1:-1]
                 from interpreter import MylangeInterpreter, CodeCleaner
                 mi:MylangeInterpreter = mylangeInterpreter
                 parts:list[str] = [item.strip() for item in CodeCleaner.split_top_level_commas(insides)]
                 ReturnS:dict = {}
+                ArchetypeTypeIds:list[LanType] = []
                 for part in parts:
                     key_value = part.split('=>', 1)
                     var = mi.format_parameter(key_value[1])
+                    assert type(var) is VariableValue
                     ReturnS[key_value[0].strip()] = var
-                return VariableValue(LanTypes.set, ReturnS)
+                    if var.Type not in ArchetypeTypeIds: 
+                        ArchetypeTypeIds.append(var.Type)
+                return VariableValue(LanType(LanScaffold.set, ArchetypeTypeIds), ReturnS)
             case _:
                 raise Exception("Typeid is Unkown.")
 
     @staticmethod
-    def get_type(string:str) -> tuple[int, int]:
+    def get_type(string:str) -> tuple['LanScaffold', int]:
         # nil
         if (string == 'nil'):
-            return (LanTypes.nil, 0)
-        # Boolean 
+            return (LanScaffold.nil, 0)
+        # boolean 
         if (string == "true"):
-            return (LanTypes.boolean, 1)
+            return (LanScaffold.boolean, 1)
         elif (string == "false"):
-            return (LanTypes.boolean, 0)
+            return (LanScaffold.boolean, 0)
         # Int 
         try:
             _ = int(string)
-            return (LanTypes.integer, 0)
+            return (LanScaffold.integer, 0)
         except: pass
         # Character
         if (string.startswith("'")) and (string.endswith("'")):
-            print("char")
-            return (LanTypes.character, 0)
+            return (LanScaffold.character, 0)
         # String 
         if (string.startswith('"')) and (string.endswith('"')):
-            return (LanTypes.string, 0)
+            return (LanScaffold.string, 0)
         # Array 
         if (string.startswith('[')) and (string.endswith(']')):
-            return (LanTypes.array, 0)
+            return (LanScaffold.array, 0)
         # Set
         if (string.startswith('(')) and (string.endswith(')')):
             if re.search(ActualRegex.SetInners.value, string) or re.search(r"\(\s*\)", string):
-                return (LanTypes.set, 0)
-            else: return (LanTypes.nil, 0)
+                return (LanScaffold.set, 0)
+            else: return (LanScaffold.nil, 0)
         # No value found, returning Nil
-        return (LanTypes.nil, 0)
+        return (LanScaffold.nil, 0)
 
 class NotValidType(Exception):
     def __init__(self, value, message="Invalid input value"):
@@ -148,21 +98,8 @@ class NotValidType(Exception):
         self.message = message
         super().__init__(self.message)
 
-TypeNameArray:list[list[str]] = [
-    ["nil"],
-    ["boolean", "bool"],
-    ["integer", "int"],
-    ["character", "char"], 
-    ["string", "str"], 
-    ["array", "arr"], 
-    ["set"], 
-    ["casting"], 
-    ["dynamic"]]
-
-class LanTypes(IntEnum):
-    # Handlers #
-    BREAK     = -1
-    CONTINUE  = -2
+@unique
+class LanScaffold(IntEnum):
     # Types #
     nil       = 0
     boolean   = 1
@@ -173,35 +110,140 @@ class LanTypes(IntEnum):
     set       = 6
     casting   = 7
     dynamic   = 8
-
-    @staticmethod
-    def is_valid_type(typeid:int):
-        return (typeid <= 6) and (typeid >= 0)
     
-    @staticmethod
-    def is_indexable(typeid:int):
-        return (typeid == LanTypes.array)
-    
-    @staticmethod
-    def from_string(typestring:str) -> int:
-        for i, aliases in enumerate(TypeNameArray):
+    @classmethod
+    def from_string(cls, typestring:str) -> int:
+        for i, aliases in enumerate(cls.TypeNameArray()):
             if typestring in aliases: return i
         return -1
 
+    @classmethod
+    def to_string_name(cls, typeid:int) -> str:
+        return cls.TypeNameArray()[typeid][0]
+    
     @staticmethod
-    def to_string_name(typeid:int) -> str:
-        return TypeNameArray[typeid][0]
+    def TypeNameArray() -> list[list[str]]:
+        return [
+            ["nil"],
+            ["boolean", "bool"],
+            ["integer", "int"],
+            ["character", "char"], 
+            ["string", "str"], 
+            ["array", "arr"], 
+            ["set"], 
+            ["casting"], 
+            ["dynamic"]
+        ]
+    
+class LanType():
+    
+    TypeNum:LanScaffold
+    Archetype:'list[LanType]|None'
+    
+    def __init__(self, typeNum:LanScaffold, archetype:'list[LanType]|None'=None) -> None:
+        self.TypeNum = typeNum
+        self.Archetype = archetype
+    
+    def __eq__(self, other:object) -> bool:
+        if type(other) is not LanType: raise Exception("Cannot compare these types!")
+        if self.TypeNum.value != other.TypeNum.value: return False
+        if (self.Archetype is None) and (other.Archetype is None): return True
+        assert self.Archetype is not None; assert other.Archetype is not None
+        if len(self.Archetype) != len(other.Archetype): return False
+        for i, archtypette in enumerate(self.Archetype):
+            if archtypette != other.Archetype[i]: return False
+        return True
+    
+    def __ne__(self, other: object) -> bool:
+        return not self.__eq__(other)
+    
+    def __str__(self) -> str:
+        return f"{LanScaffold.TypeNameArray()[self.TypeNum]}" + (f"<{self.Archetype}>" if self.Archetype else "")
+    
+    # Static types that are always the same
+    @staticmethod
+    def nil(): return LanType(LanScaffold.nil)
+    
+    @staticmethod
+    def bool(): return LanType(LanScaffold.boolean)
+    
+    @staticmethod
+    def int(): return LanType(LanScaffold.integer)
+    
+    @staticmethod
+    def char(): return LanType(LanScaffold.character)
+    
+    @staticmethod
+    def str(): return LanType(LanScaffold.string)
 
 class ParamChecker:
     @staticmethod
-    def EnsureIntegrety(*params:tuple[VariableValue, int]) -> bool:
-        for param in params:
-            if param[1] != param[0].typeid: raise Exception(f"[Type Validation] Type mismatch: Expected {TypeNameArray[param[1]]}, got {TypeNameArray[param[0].typeid]}.")
+    def EnsureIntegrety(*params:tuple['VariableValue', int]) -> bool:
+        # for param in params:
+        #     if param[1] != param[0].typeid: 
+        #         raise Exception(f"[Type Validation] Type mismatch: Expected {LanScaffold.TypeNameArray()[param[1]]}, got {LanScaffold.TypeNameArray()[param[0].typeid]}.")
         return True
-    
+
     @staticmethod
-    def GetTypesOfParameters(*params:VariableValue) -> list[LanTypes]:
+    def GetTypesOfParameters(*params:'VariableValue') -> list[LanType]:
         Return = []
         for param in params:
-            Return.append(param.typeid)
+            Return.append(param.Type)
         return Return
+
+class VariableValue:
+    type VariableValueLike = None|bool|int|str|list['VariableValue']|dict[str,'VariableValue']|'LanClass'
+    Type:LanType
+    def __init__(self, typeStruct:LanType, value:VariableValueLike=None):
+        self.Type = typeStruct
+        self.value:VariableValue.VariableValueLike = None
+        if (value != None): self.value = value
+
+    def __str__(self):
+        match (self.Type.TypeNum):
+            case LanScaffold.nil:
+                return 'nil'
+            case LanScaffold.boolean:
+                return f"{self.value}".lower()
+            case LanScaffold.integer:
+                return f"{self.value}"
+            case LanScaffold.character:
+                return f"'{self.value}'"
+            case LanScaffold.string:
+                return f'"{self.value}"'
+            case LanScaffold.array:
+                assert type(self.value) is list
+                larray = [f"{item}" for item in self.value]
+                return f"[{', '.join(larray)}]"
+            case LanScaffold.set:
+                assert type(self.value) is dict
+                lset = [f"{k}:{v}" for k, v in self.value.items()]
+                return f"({', '.join(lset)})"
+        return f"<{self.value}@{self.Type}>"
+    
+    def __repr__(self) -> str:
+        return self.__str__()
+    
+    def isof(self, type:LanType) -> bool:
+        return self.Type == type
+    
+    def to_string(self):
+        match (self.Type.TypeNum):
+            case LanScaffold.boolean:
+                return f"{self.value}".lower()
+            case LanScaffold.integer:
+                return f"{self.value}"
+            case LanScaffold.character | LanScaffold.string:
+                return self.value
+            case _:
+                return self.__str__()
+            
+    def colon_access(self, index:str) -> 'VariableValue':
+        ParamChecker.EnsureIntegrety((self, LanScaffold.set))
+        assert type(self.value) is dict
+        return self.value[index]
+
+    def bracket_access(self, index:int) -> 'VariableValue':
+        ParamChecker.EnsureIntegrety((self, LanScaffold.array))
+        assert type(self.value) is list
+        return self.value[index]
